108c5db3e84f325922f5469ff920253cd80a6969
Bariscan Ceken <bariscanceken@hotmail.com>
2024-10-25 22:28:32 +0300

    Colors And Text Options Changed.

--- catchtheturtle.py
+++ catchtheturtle.py
@@ -5,13 +5,14 @@ import time
 
 #options screen
 drawboard = turtle.Screen()
-drawboard.bgcolor("white")
+drawboard.bgcolor("light blue")
 drawboard.title("Turtle Game")
 
 #options ti1
 ti = turtle.Turtle("turtle")
 ti.shapesize(3)
-ti.color("blue")
+ti.color("green")
+
 
 #options ti2
 ti2 = turtle.Turtle()
@@ -19,26 +20,27 @@ ti2.shapesize(3)
 ti2.color("Red")
 
 #
-a = 0
+score = 0
 xcrd = 0
 ycrd = 0
 
-#Ne kadar saniye istiyorsanız 10 katını giriniz.
-t = 600
+#Yazacak Turtle'ı Gizle
+ti2.hideturtle()
+
+#Ne kadar saniye istiyorsanız 5/3 katını giriniz.
+t = 60
 
 #random
 def randomcoordinate(x,y):
     global xcrd
     global ycrd
-    global a
+    global score
     global t
     if abs(x - xcrd) < 50 and abs(y - ycrd) < 50:
-        a += 1
+        score += 1
         ti2.teleport(-650,350)
     else :
         ti2.teleport(-650,350)
-
-
 
     print(f"Tıklanan: ({x}, {y}), Rastgele: ({xcrd}, {ycrd})")
     xcrd = random.randrange(-600,600)
@@ -53,11 +55,11 @@ while t > 0 :
     ti2.clear()
     t -= 1
     ti2.teleport(0,350)
-    ti2.write(f"Kalan Süre : {t}") 
+    ti2.write(f"Kalan Süre : {t}", align="center", font=("Arial", 32, "bold"))
     ti2.teleport(-650,350)
-    ti2.write(f"Skorunuz: {a}")
+    ti2.write(f"Skorunuz: {score}", align="center", font=("Arial", 32, "bold"))
     time.sleep(0.1)
 
-print("Toplam Skorunuz: ",a)
+print("Toplam Skorunuz: ",score)
 exit()
 turtle.done()
