22c35b2b55d73024f41300c334d0e3cac45974a6
Bariscan Ceken <bariscanceken@hotmail.com>
2024-10-25 19:42:44 +0300

    Fixed Proximity To Cursor And Score.

--- catchtheturtle.py
+++ catchtheturtle.py
@@ -2,34 +2,48 @@
 import turtle
 import random
 
-#options
+#
+a = 0
+xcrd = 0
+ycrd = 0 
+
+#options screen
 drawboard = turtle.Screen()
 drawboard.bgcolor("white")
 drawboard.title("Turtle Game")
+
+#options ti1
 ti = turtle.Turtle("turtle")
 ti.shapesize(3)
 ti.color("blue")
 
-ti.teleport(-650,350)
-skor = 0
-ti.write(f"Skorunuz: {skor}")
+#options ti2
+ti2 = turtle.Turtle()
+ti2.shapesize(3)
+ti2.color("Red")
+
 
 #random
 def randomcoordinate(x,y):
     global xcrd
     global ycrd
+    global a
+    ti2.clear()
+    if abs(x - xcrd) < 50 and abs(y - ycrd) < 50:
+        a += 1
+        ti2.teleport(-650,350)
+        ti2.write(f"Skorunuz: {a}")
+    else :
+        ti2.teleport(-650,350)
+        ti2.write(f"Skorunuz: {a}")
+
+    print(f"Tıklanan: ({x}, {y}), Rastgele: ({xcrd}, {ycrd})")
     xcrd = random.randrange(-600,600)
     ycrd = random.randrange(-300,300)
     ti.teleport(xcrd,ycrd)
-    if abs(x-xcrd) < 50 or abs(y-ycrd) < 50:
-         updateskor()
 
-#skor
-def updateskor():   
-    global skor
-    skor += 1
-
-
+        
+#Click func
 drawboard.onscreenclick(randomcoordinate)
 
 turtle.done()
