4ff0a75fdce6234547f8e9e423c0f7360e490483
Bariscan Ceken <bariscanceken@hotmail.com>
2024-10-25 20:49:55 +0300

    Time Module And Finish Project.

--- catchtheturtle.py
+++ catchtheturtle.py
@@ -1,11 +1,7 @@
 #library
 import turtle
 import random
-
-#
-a = 0
-xcrd = 0
-ycrd = 0 
+import time
 
 #options screen
 drawboard = turtle.Screen()
@@ -22,30 +18,46 @@ ti2 = turtle.Turtle()
 ti2.shapesize(3)
 ti2.color("Red")
 
+#
+a = 0
+xcrd = 0
+ycrd = 0
+
+#Ne kadar saniye istiyorsanız 10 katını giriniz.
+t = 600
 
 #random
 def randomcoordinate(x,y):
     global xcrd
     global ycrd
     global a
-    ti2.clear()
+    global t
     if abs(x - xcrd) < 50 and abs(y - ycrd) < 50:
         a += 1
         ti2.teleport(-650,350)
-        ti2.write(f"Skorunuz: {a}")
     else :
         ti2.teleport(-650,350)
-        ti2.write(f"Skorunuz: {a}")
+
+
 
     print(f"Tıklanan: ({x}, {y}), Rastgele: ({xcrd}, {ycrd})")
     xcrd = random.randrange(-600,600)
     ycrd = random.randrange(-300,300)
-    ti.teleport(xcrd,ycrd)
+    ti.teleport(xcrd,ycrd)   
 
-        
-#Click func
-drawboard.onscreenclick(randomcoordinate)
+#Click Çalışması
+drawboard.onscreenclick(randomcoordinate) 
 
+#Zaman Döngüsü
+while t > 0 :
+    ti2.clear()
+    t -= 1
+    ti2.teleport(0,350)
+    ti2.write(f"Kalan Süre : {t}") 
+    ti2.teleport(-650,350)
+    ti2.write(f"Skorunuz: {a}")
+    time.sleep(0.1)
+
+print("Toplam Skorunuz: ",a)
+exit()
 turtle.done()
-
-
